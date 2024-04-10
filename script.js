ec116d4d3e51ff8aebb94083c090af3ba16fb38b
Aadith2022 <aadith.samala@stonybrook.edu>
2024-04-10 10:11:35 -0400

    graph things

--- script.js
+++ script.js
@@ -14,6 +14,25 @@ async function fetchPatientData() {
     }
 }
 
+async function fetchSampleData() {
+
+  try {
+
+    const response = await fetch("https://api.cellmodelpassports.sanger.ac.uk/samples?page[size]=10000")
+
+    if (!response.ok) {
+      throw new Error("Could not fetch sample resources");
+    }
+
+    const data = await response.json()
+    return data
+
+  } catch(error) {
+    console.log(error)
+  }
+
+}
+
 async function xSelected() {
 
     var pd = await fetchPatientData()
@@ -55,6 +74,144 @@ async function xSelected() {
 
 }
 
+async function sxSelected() {
+
+  var sd = await fetchSampleData()
+
+  console.log("sample function is getting hit")
+
+  var xSelect = document.getElementById('sx_drop');
+  var xVal = document.getElementById('sx_val_drop');
+  var ySelect = document.getElementById('sy_drop');
+
+  var xx = xSelect.value;
+
+  xVal.innerHTML = '';
+  ySelect.innerHTML = ''
+
+  if (xx) {
+
+      val_options = [...new Set(sd.data.filter(item => item.attributes[xx] !== "NA" && item.attributes[xx] !== "Unknown" && item.attributes[xx] != null).map(item => item.attributes[xx]))];
+
+      val_options.forEach(value => {
+          var option = document.createElement("option");
+          option.text = value;
+          option.value = value;
+          xVal.add(option);
+        });
+
+        y_options = Object.keys(sd.data[0].attributes).filter(item => item != xx)
+
+        y_options.forEach(value => {
+
+          var option = document.createElement("option");
+          option.text = value;
+          option.value = value;
+          ySelect.add(option)
+
+        })
+
+  }
+
+}
+
+async function sample_patients() {
+
+  var pd = await fetchPatientData()
+  var sd = await fetchSampleData()
+
+  var xSelect = document.getElementById('x_drop');
+  var xv_Select = document.getElementById('x_val_drop');
+  var ySelect = document.getElementById('y_drop');
+
+  var div = document.getElementById('sample_info_container')
+  div.style.display = "block";
+
+  xx = xSelect.value
+  xval = xv_Select.value
+  yy = ySelect.value
+
+  id_list = pd.data.filter(item => item.attributes[xx] == xval && item.attributes[yy] != "null").map(item => item.id)
+
+  console.log("This is the id_list:")
+  console.log(id_list)
+
+  var x_options = Object.keys(sd.data[0].attributes)
+
+  // Get the dropdown element
+  var x_dropdown = document.getElementById('sx_drop');
+
+  // Loop through the list and create options
+  for (var i = 0; i < x_options.length; i++) {
+      var option = document.createElement('option');
+      option.value = x_options[i];
+      option.text = x_options[i];
+      x_dropdown.add(option);
+  }
+
+}
+
+async function sample_generate() {
+
+  var pd = await fetchPatientData()
+  var sd = await fetchSampleData()
+
+  var xSelect = document.getElementById('x_drop');
+  var xv_Select = document.getElementById('x_val_drop');
+  var ySelect = document.getElementById('y_drop');
+
+  //clean this up, messy just for now
+  var sxSelect = document.getElementById('sx_drop');
+  var sxv_Select = document.getElementById('sx_val_drop');
+  var sySelect = document.getElementById('sy_drop');
+
+  xx = xSelect.value
+  xval = xv_Select.value
+  yy = ySelect.value
+
+  sxx = sxSelect.value
+  sxval = sxv_Select.value
+  syy = sySelect.value
+
+  if (sxx === "" || sxval === "" || syy === "") {
+    alert("Please select options in all dropdowns");
+  } else {
+
+    id_list = pd.data.filter(item => item.attributes[xx] == xval && item.attributes[yy] != "null").map(item => item.id)
+  
+    console.log("This is the id_list:")
+    console.log(id_list)
+
+    const trace = [{
+
+        x: sampled = sd.data.filter(item => id_list.includes(item.id)).filter(item => item.attributes[sxx] == sxval && item.attributes[syy] != "null").map(item => item.attributes[syy]),
+        type: 'histogram',
+        
+      }];
+
+    layout = [{
+
+        title: syy.replace(/_/g, ' ') + " Vs " + sxx.replace(/_/g, ' '),
+        xaxis: {
+          title: syy.replace(/_/g, ' ')
+        },
+        yaxis: {
+          title: "# of " + sxval.replace(/_/g, ' ')
+        },
+        
+      }];
+
+    const div = document.createElement('div');
+    Plotly.newPlot(div, trace, layout[0]);
+
+    const displayDiv = document.querySelector('#data_graphs');
+    displayDiv.innerHTML = "";
+    displayDiv.appendChild(div);
+
+  }
+
+}
+
 
 async function generate() {
 
@@ -93,6 +250,9 @@ async function generate() {
 
         const div = document.createElement('div');
         Plotly.newPlot(div, trace, layout[0]);
+
+        var sample = document.getElementById("more_info");
+        sample.style.display = "block";
 
         const displayDiv = document.querySelector('#data_graphs');
         displayDiv.innerHTML = "";
